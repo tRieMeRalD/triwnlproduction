8df743899a3c8554d0a724e16c2b2c0a457a75be
unknown <horizon.william14@gmail.com>
2019-04-10 23:39:11 -0400

    Apr 10 4th commit

--- client/src/store.js
+++ client/src/store.js
@@ -8,10 +8,11 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
+  /* compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  ) */
+  applyMiddleware(...middleware)
 );
 
 export default store;
