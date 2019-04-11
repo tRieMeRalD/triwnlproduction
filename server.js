52f136928a393b48c43abe8a6554eee136c4ff71
unknown <horizon.william14@gmail.com>
2019-04-10 23:33:12 -0400

    Apr 10 3rd commit

--- server.js
+++ server.js
@@ -35,12 +35,11 @@ app.use("/api/admin", admin);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  //app.use(express.static("client/build"));
-  app.use("/static", express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
 
-  /* app.get("*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  }); */
+  });
 }
 
 const port = process.env.PORT || 5000;
